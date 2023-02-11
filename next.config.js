87cfb1e44e19f850cfbdc18e661e5f105f75cc33
Irham Maulana <i.maulana.azn@gmail.com>
2023-02-11 17:29:14 +0700

    changing env and next config to use railway link

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
-module.exports = { images: { domains: ['localhost'], formats: ['image/avif', 'image/webp', 'image/jpg', 'image/jpeg', 'image/png'] } };
+module.exports = { images: { domains: ['storegg-server.up.railway.app'], formats: ['image/avif', 'image/webp', 'image/jpg', 'image/jpeg', 'image/png'] } };

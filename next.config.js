fa1571c0357ca223164d248dfe21709452d04e69
Irham Maulana <i.maulana.azn@gmail.com>
2023-02-11 17:54:51 +0700

    replace dbl quote with single quote on nextjs config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line quotes
-module.exports = { images: { domains: ["storegg-server.up.railway.app"], formats: ["image/jpeg", "image/png"] } };
+module.exports = { images: { domains: ['storegg-server.up.railway.app'], formats: ['image/jpeg', 'image/png'] } };

0705b80014d3efd0ec9c5423e823dbe59f6b32f2
Irham Maulana <i.maulana.azn@gmail.com>
2023-06-18 10:21:46 +0700

    change the API env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//   images: {
-//     domains:
-//     ['storegg-server.up.railway.app'],
-//     formats: ['image/jpeg', 'image/png'],
-//   },
-// };
 module.exports = {
-  images:
-    { domains: ['localhost'], formats: ['image/jpeg', 'image/png'] },
+  images: {
+    domains:
+    ['storegg-server-production.up.railway.app'],
+    formats: ['image/jpeg', 'image/png'],
+  },
 };
+// module.exports = {
+//   images:
+//     { domains: ['localhost'], formats: ['image/jpeg', 'image/png'] },
+// };

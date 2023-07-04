26fc8a2569aef1b08399a61d74d62a88fce030f3
Irham Maulana <i.maulana.azn@gmail.com>
2023-07-04 10:39:51 +0700

    change nextjs config for image format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
     domains: ['storegg-server-production.up.railway.app'],
-    formats: ['image/jpg', 'image/jpeg', 'image/png', 'image/avif', 'image/webp'],
+    formats: ['image/jpeg', 'image/png'],
   },
 };

78fd23bcf1223f54e4b2b44cc30eb3ac898fc77c
Irham Maulana <i.maulana.azn@gmail.com>
2023-07-04 10:36:40 +0700

    changing next config for image

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 module.exports = {
   images: {
     domains: ['storegg-server-production.up.railway.app'],
-    formats: [
-      'image/jpeg',
-      'image/png',
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/:path*',
-        destination: 'https://storegg-server.up.railway.app/api/v1/:path*', // Replace with your target URL
-      },
-    ];
+    formats: ['image/jpg', 'image/jpeg', 'image/png', 'image/avif', 'image/webp'],
   },
 };
-// module.exports = {
-//   images:
-//     { domains: ['localhost'], formats: ['image/jpeg', 'image/png'] },
-// };
